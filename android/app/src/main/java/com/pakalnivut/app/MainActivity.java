952f6b2d41da2fb50501545cac067e901813cff8
Nitay Shporn <nshporn@gmail.com>
2025-07-28 16:18:51 +0300

    Initial commit

---
+++ android/app/src/main/java/com/pakalnivut/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pakalnivut.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pakalnivut.app',
+  appName: 'פק״ל ניווט',
+  webDir: 'build'
+};
+
+export default config;
